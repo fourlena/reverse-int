7db1095a1d867364b351521109790f18d96684b6
Elena Pavlovich <evapetrovmanted@gmail.com>
2020-10-20 18:40:28 +0300

    feat: upload function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        var new_stinrg = n.toString().split('').reverse();
+        var pup = new_stinrg.pop()
+       return parseInt(new_stinrg.join(''));
+    }
+    return parseInt( n.toString().split('').reverse().join('')); 
 }
